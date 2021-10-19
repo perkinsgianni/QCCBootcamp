4de3e2c6b61abda4934876e6aa67e49a0db38709
gianni p <86334418+perkinsgianni@users.noreply.github.com>
2021-10-18 20:20:31 -0400

    Add files via upload

---
+++ Assignment4.js
@@ -0,0 +1,37 @@
+// BMI calculator
+
+let lucasMass = 78
+let lucasHeight = 1.69
+let johnMass = 92
+let johnHeight = 1.95
+
+const lucasBMI = lucasMass / (lucasHeight * lucasHeight)
+const johnBMI = johnMass / (johnHeight * johnHeight)
+
+const lucasHigherBMI = lucasBMI > johnBMI
+
+console.log(`Lucas' BMI is ${Math.round(lucasBMI)}.`)
+console.log(`John's BMI is ${Math.round(johnBMI)}.`)
+console.log(`Lucas' BMI is higher than John's BMI, that is ${lucasHigherBMI}.`)
+
+
+// Temperature Converter
+
+var tempCelsius = 11
+var tempFahrenheit = (tempCelsius * 9/5) + 32
+console.log(`${tempCelsius}°C is ${Math.round(tempFahrenheit)}°F.`)
+
+var tempFahrenheit = 64
+var tempCelsius = (tempFahrenheit - 32) * 5/9
+console.log(`${tempFahrenheit}°F is ${Math.round(tempCelsius)}°C.`)
+
+
+// BMI statements
+
+console.log(`Lucas' BMI is higher than John's!`)
+
+if (lucasBMI > johnBMI) {
+    console.log(`Lucas' BMI (${Math.round(lucasBMI)}) is higher than John's BMI (${Math.round(johnBMI)}).`);
+} else {
+    console.log(`John's BMI (${Math.round(johnBMI)}) is higher than Lucas' BMI (${Math.round(lucasBMI)}).`)
+}
